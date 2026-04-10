a635e4e9387e2a9864a6618f931273bf85eaee73
Aminul islam <aminurrohman98@gmail.com>
2026-04-10 23:31:33 +0100

    made home page , navbar and attach them in a path

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import RootLayout from "./layout/RootLayout";
+import RootLayout from "./layout/RootLayout.jsx";
+import HomePage from "./pages/home/HomePage.jsx";
+import AppsPage from "./pages/apps/AppsPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: HomePage,
+      },
+      {
+        path: "/appsPage",
+        Component: AppsPage,
+      },
+    ],
+    errorElement: (
+      <h1 className="w-full min-h-screen flex items-center justify-center font-semibold text-2xl text-rose-600 ">
+        Not found the page
+      </h1>
+    ),
   },
 ]);
 
